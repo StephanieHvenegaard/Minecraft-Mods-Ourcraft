9dd5fa19979ad9b23c5a0e377881b77fa2af4356
Stephanie Night <s.hvenegaard@outlook.com>
2020-06-17 16:30:52 +0200

    did not work

--- src/main/java/com/thenights/ourcraft/util/Reference.java
+++ src/main/java/com/thenights/ourcraft/util/Reference.java
@@ -1,8 +1,8 @@
 package com.thenights.ourcraft.util;
 
 public class Reference {
-    public static final String MODID = "examplemod";
-    public static final String NAME = "Example Mod";
+    public static final String MODID = "ourcraft";
+    public static final String NAME = "Ourcraft Mod";
     public static final String VERSION = "1.0";
     public static final String Client ="com.thenights.ourcraft.util.proxy.ClientProxy";
     public static final String Common ="com.thenights.ourcraft.util.proxy.CommonProxy";
