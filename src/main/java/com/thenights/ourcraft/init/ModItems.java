def70fa1aac420197b914b8dc1675204376d468c
Stephanie Night <s.hvenegaard@outlook.com>
2020-07-11 17:57:13 +0200

    added, graphics and language

--- src/main/java/com/thenights/ourcraft/init/ModItems.java
+++ src/main/java/com/thenights/ourcraft/init/ModItems.java
@@ -1,7 +1,8 @@
 package com.thenights.ourcraft.init;
 
 import com.thenights.ourcraft.Ourcraft;
-import com.thenights.ourcraft.objects.item.BaseItem;
+import com.thenights.ourcraft.objects.item.*;
+import com.thenights.ourcraft.util.ModMaterials;
 import net.minecraft.block.Block;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
@@ -16,13 +17,20 @@ import java.util.ArrayList;
 
 public class ModItems {
     public static Item OBSIDIAN_INGOT_ITEM;
+    public static Item OBSIDIAN_SWORD_ITEM;
+    public static Item OBSIDIAN_SHOVEL_ITEM;
+    public static Item OBSIDIAN_PICKAXE_ITEM;
+    public static Item OBSIDIAN_AXE_ITEM;
+    public static Item OBSIDIAN_HOE_ITEM;
     public static ArrayList<Item> ITEMS = new ArrayList<>();
 
     public static void PreInit() {
-        
         OBSIDIAN_INGOT_ITEM =  new BaseItem("ingot_obsidian");
-
-        ITEMS.add(OBSIDIAN_INGOT_ITEM);
+        OBSIDIAN_PICKAXE_ITEM = new BasePickaxeItem("pickaxe_obsidian", ModMaterials.OBSIDIAN);
+        OBSIDIAN_SHOVEL_ITEM = new BaseShovelItem("shovel_obsidian",ModMaterials.OBSIDIAN);
+        OBSIDIAN_AXE_ITEM = new BaseAxeItem("axe_obsidian", ModMaterials.OBSIDIAN);
+        OBSIDIAN_HOE_ITEM = new BaseHoeItem("hoe_obsidian",ModMaterials.OBSIDIAN);
+        OBSIDIAN_SWORD_ITEM = new BaseSwordItem("sword_obsidian",ModMaterials.OBSIDIAN);
     }
 
     public static void Init() {
--- src/main/java/com/thenights/ourcraft/objects/item/BaseAxeItem.java
+++ src/main/java/com/thenights/ourcraft/objects/item/BaseAxeItem.java
@@ -2,17 +2,16 @@ package com.thenights.ourcraft.objects.item;
 
 import com.thenights.ourcraft.Ourcraft;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemSword;
+import net.minecraft.item.ItemAxe;
 
 import static com.thenights.ourcraft.init.ModItems.ITEMS;
 
-public class BaseSwordItem  extends ItemSword {
-    public BaseSwordItem(String name, Item.ToolMaterial material) {
-        super(material);
+public class BaseAxeItem extends ItemAxe {
+    public BaseAxeItem(String name, Item.ToolMaterial material) {
+        super(material,7.0F, -3.2F);
         setCreativeTab(Ourcraft.CORE_TAB);
         setRegistryName(Ourcraft.RESOURCE_PREFIX + name);
         setUnlocalizedName(this.getRegistryName().toString());
         ITEMS.add(this);
     }
-
 }
