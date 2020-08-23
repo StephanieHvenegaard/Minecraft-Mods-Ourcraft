36b481018cac1fde6e31739df48e47ab309617b3
Stephanie Night <s.hvenegaard@outlook.com>
2020-08-23 12:04:09 +0200

    added dense obsidian dust, tweek recipies

--- src/main/java/com/thenights/ourcraft/init/ModItems.java
+++ src/main/java/com/thenights/ourcraft/init/ModItems.java
@@ -17,7 +17,9 @@ import net.minecraftforge.registries.IForgeRegistryEntry;
 import java.util.ArrayList;
 
 public class ModItems {
+    public static Item OBSIDIAN_DUST_ITEM;
     public static Item OBSIDIAN_INGOT_ITEM;
+
     public static Item OBSIDIAN_SWORD_ITEM;
     public static Item OBSIDIAN_SHOVEL_ITEM;
     public static Item OBSIDIAN_PICKAXE_ITEM;
@@ -32,6 +34,7 @@ public class ModItems {
     public static ArrayList<Item> ITEMS = new ArrayList<>();
 
     public static void PreInit() {
+        OBSIDIAN_DUST_ITEM =  new BaseItem("dust_obsidian");
         OBSIDIAN_INGOT_ITEM =  new BaseItem("ingot_obsidian");
         OBSIDIAN_PICKAXE_ITEM = new BasePickaxeItem("pickaxe_obsidian", ModMaterials.OBSIDIAN);
         OBSIDIAN_SHOVEL_ITEM = new BaseShovelItem("shovel_obsidian",ModMaterials.OBSIDIAN);
@@ -46,7 +49,7 @@ public class ModItems {
     }
 
     public static void Init() {
-        GameRegistry.addSmelting(Blocks.OBSIDIAN, new ItemStack(OBSIDIAN_INGOT_ITEM), 3.0F);
+        GameRegistry.addSmelting(OBSIDIAN_DUST_ITEM, new ItemStack(OBSIDIAN_INGOT_ITEM), 3.0F);
         //GameRegistry.addSmelting((Block)ModBlocks.vulcaniteOre, new ItemStack(vulcaniteNuggetItem), 1.0F);
 
         OreDictionary.registerOre("ingotObsidian", OBSIDIAN_INGOT_ITEM);
